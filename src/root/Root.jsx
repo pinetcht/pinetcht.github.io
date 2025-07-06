a63d6f6f6ec9ddf34719925f0c7a83f93b5b0b37
Pine Netcharussaeng <pinetcht@gmail.com>
2025-07-06 12:08:19 +0700

    delete the test text

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -36,7 +36,6 @@ export const Root = () => {
       <div className="intro">
         <div className="description">
           <div className="title">
-            <h1>test</h1>
             <h1>Hi, I am Pine,  {selectedOption === 'optimizer' ? 'an' : 'a'} </h1>
             <Dropdown options={titles} onSelect={handleSelect}></Dropdown>
 
